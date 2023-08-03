b540002444256685419a0c378d519b89bf2dd77f
mkln23 <mukilans232003@gmail.com>
2023-08-03 13:22:17 +0530

    inception added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import Card from './MovieDet';
+import './MovieDet.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reac
-        </a>
-      </header>
-      {/* <Card /> */}
-    </div>
-  );
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn Reac
+    //     </a>
+    //   </header>
+    // </div>
+    <Card movie="Inception" dir="Christopher Nolan" genre="Sci-fi" link="https://www.imdb.com/title/tt1375666/"/>
+    );
 }
 
 export default App;
