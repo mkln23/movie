0950118c0832d82047e3ae50296409393ede39e6
mkln23 <mukilans232003@gmail.com>
2023-08-03 15:53:32 +0530

    img not inserted

---
+++ src/MovieDet.js
@@ -0,0 +1,53 @@
+import './MovieDet.css';
+import './images/inception.jpg';
+import './images/eot.jpg';
+
+// import img from './images/eot.jpg'
+// import './images/'
+// // import inception from './images/inception.jpg';
+// // import picChoose from './MoviePic';
+
+function pic(movie){
+  var path;
+  switch(movie){
+      case "Inception":{ path="./images/inception.jpg"; break; }
+      case "Edge Of Tomorrow":{ path="./images/eot.jpg"; break; }
+         default : {path="";break;}
+  }
+  return path;
+}
+
+function Card(props){
+    return (
+        <>
+  <div className="movie">
+    <div className="col">
+      <div className="image">
+        <img src= {pic(props.movie)} alt="Movie" />
+      </div>
+    </div>
+    <div className="col">
+      <div className="row">
+        <div className="bold">Movie : &nbsp;</div>
+        <div className="movie_name">{props.movie}</div>
+      </div>
+      <div className="row">
+        <div className="bold">Director : &nbsp;</div>
+        <div className="dir">{props.dir}</div>
+      </div>
+      <div className="row">
+        <div className="bold">Genre :&nbsp;</div>
+        <div className="plot">{props.genre}</div>
+      </div>
+      <div className="row">
+        {" "}
+        <span>Know more at &nbsp; </span>
+        <a href={props.link}>Imdb</a>
+      </div>
+    </div>
+  </div>
+</>
+    )
+}
+
+export default Card;
